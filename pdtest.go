fa8eea3aa1a4a12a3ead77d566e2c66c52b68e21
helmar <helmar@localhost>
2009-11-14 16:48:08 -0500

    Added comment about how to use the example.

--- pdtest.go
+++ pdtest.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-  pd := pdfread.Load("tvlvolltext.pdf");
+  pd := pdfread.Load("tvlvolltext.pdf"); // change to your file name here
   if pd != nil {
     fmt.Printf("--%s--\n", pd.Obj(pd.Trailer["/Root"]));
     fmt.Printf("--%s--\n", pd.Obj(pd.Trailer["/Root"]));
